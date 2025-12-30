e4c84cf4b59a75a3584c24576980978d08f07c01
Bommada DurgaPrasad <durgaprasad79423@gmail.com>
2025-12-30 14:21:09 +0530

    Update TransportpassmanagementsystemApplication.java

--- src/main/java/com/example/transportpassmanagementsystem/TransportpassmanagementsystemApplication.java
+++ src/main/java/com/example/transportpassmanagementsystem/TransportpassmanagementsystemApplication.java
@@ -2,8 +2,12 @@ package com.example.transportpassmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(
+		pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class TransportpassmanagementsystemApplication {
 
 	public static void main(String[] args) {
